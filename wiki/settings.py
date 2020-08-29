e9d867d18bf4a432934eb22cc38bd37ac6bb3fa1
Shrijak Dahal <dahalshrijak@gmail.com>
2020-08-29 12:03:10 +0545

    Update settings.py

--- wiki/settings.py
+++ wiki/settings.py
@@ -83,23 +83,11 @@ WSGI_APPLICATION = 'wiki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'USER': 'shrijak',
-        'PASSWORD': '43ae030e78',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
 }
 
 
